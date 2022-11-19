24f13707fff957110a6c81cf74c545a9166e7adc
capitolcomputers <aijalonbode@gmail.com>
2022-11-19 03:03:25 +0100

    fixed smooth scrool, Only navbar left

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
 
 const viewPlansBtn = document.querySelector('.view-plans');
 const section1El = document.querySelector('.section-1');
-
+const viewPlansBtn2 = document.querySelector('.view-plans-2');
 
 viewPlansBtn.addEventListener('click', function (e) {
   e.preventDefault()
   
   section1El.scrollIntoView({behavior: 'smooth'})
+})
+
+viewPlansBtn2.addEventListener('click', function (e) {
+    e.preventDefault();
+     section1El.scrollIntoView({ behavior: 'smooth' });
 })
